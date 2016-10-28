bde4eefe01353a9e62565cf5202c01fc7a578eba
Florian Guigue <florian.guigue@etu.univ-lyon1.fr>
2016-10-28 13:46:27 +0200

    Delete Index.php

--- Index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    require_once 'Velov.php';
-    $velovStation = new Velov();
-    $velovStation->getStationState();
-    $velovStation->populateDB();
-    $velovStation->printStations();
