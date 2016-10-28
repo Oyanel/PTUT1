03523adc0709852f0635145b83e37b2dd3b940e3
Florian Guigue <florian.guigue@etu.univ-lyon1.fr>
2016-10-28 17:00:12 +0200

    Add files via upload

--- js/graph.js
+++ js/graph.js
@@ -1,38 +1,28 @@
+var villeurbanne = 0;
+var lyon7 = 0;
+for (var i = 0; i < station.length; i++) {
+  if (station[i].commune == 'VILLEURBANNE') {
+    villeurbanne += 1;
+  }
+  if (station[i].commune == 'Lyon 7 ème') {
+    lyon7 += 1;
+  }
+}
 
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ["Commune", "Nombres de stations"],
+          ['Villeurbanne', villeurbanne],
+          ['Lyon 7', lyon7]]);
+
+        var options = {
+          title: 'Nombre de stations vélo\'v par commune',
+          legend: { position: 'none' },
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
