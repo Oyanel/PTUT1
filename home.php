9143d0b388900529c1bd876a83bc23aea1054cb1
Oyanel <therence.brune@gmail.com>
2017-01-13 23:18:21 +0100

    nouveau css et hierarchy des dossiers

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <?php  include_once './head.html'?>
+    <body>
+    <div class="background-image"></div>
+        <div class="content">
+            <?php  include_once './header.html'?>
+            <div id="liste"></div>
+            <?php  include_once './footer.html'?>
+        </div>
+    </body>
+</html>
