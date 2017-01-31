df9047436fdf51bac4bbf06e1a801dde5fcb8829
Oyanel <th.brune@laposte.net>
2017-01-31 16:12:32 +0100

    correction moyenne place / jour

--- moyennePlacesJournee.php
+++ moyennePlacesJournee.php
@@ -3,13 +3,14 @@ include_once 'cronTask/Database.php';
 $conn = Database::getInstance();
 $conn->set_charset("utf8");
 if(!empty($conn)) {
-    $stationID = intval($_POST['id']);
-    $sommeMoyenne = array();
+    $stationID = 10063;//intval($_POST['id']);
+    $bikeAvailablePerHour = array();
     for($h=0;$h<24;$h++) {
+        $sommeMoyenne = array();
         for ($i = 0; $i < 12; $i++) {
             $multiple = 7 * $i;
             $query =
-                "SELECT avg('available_bike_stands') as moyenne
+                "SELECT avg(`available_bike_stands`) as moyenne
                 FROM Station
                 WHERE last_update >= SUBDATE( SUBDATE( ADDDATE(CURDATE(),INTERVAL ".$h." HOUR) , " . $multiple . " ) , INTERVAL 20 MINUTE )
                 AND last_update <= ADDDATE( SUBDATE( ADDDATE( CURDATE(), INTERVAL ".$h." HOUR) , " . $multiple . " ) , INTERVAL 20 MINUTE )
@@ -17,10 +18,12 @@ if(!empty($conn)) {
                 LIMIT 20;";
             $results = $conn->query($query);
             foreach ($results as $result) {
-                array_push($sommeMoyenne,intval($result['moyenne']));
+                if(!empty($result['moyenne'])) {
+                    array_push($sommeMoyenne, intval($result['moyenne']));
+                }
             }
         }
-        $moyenne = array_sum($sommeMoyenne)/12;
+        $moyenne = array_sum($sommeMoyenne)/count($sommeMoyenne);
         array_push($bikeAvailablePerHour ,$moyenne);
     }
     echo json_encode($bikeAvailablePerHour);
